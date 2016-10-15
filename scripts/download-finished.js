79465fe8278ef53b420cdfde63f805391e9ea2fb
Grant Holle <hollegrant@gmail.com>
2016-10-15 08:29:48 +0800

    Moving start, stop, and finished scripts to a new directory

--- scripts/download-finished.js
+++ scripts/download-finished.js
@@ -2,16 +2,16 @@
 'use strict'
 
 const torrentId = process.env.TR_TORRENT_ID,
-      winston = require('./lib/logger'),
+      winston = require('../lib/logger'),
       fs = require('fs'),
-      showsDb = require('./lib/shows-db'),
+      showsDb = require('../lib/shows-db'),
       sanitize = require('sanitize-filename'),
-      notify = require('./lib/pushbullet'),
-      label = require('./lib/show-label')
+      notify = require('../lib/pushbullet').downloadFinished,
+      label = require('../lib/show-label')
 
 showsDb.db.get('select * from downloads where transmission_id = ?', [torrentId], (err, show) => {
   const msg = `${show.show} ${label(show.season, show.episode)}, ${show.episode_name}, has finished downloading.`
 
   winston.info(msg)
-  notify(`Pete says, "Torrent finished!"`, msg)
+  notify(msg)
 })
