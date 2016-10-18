9eba7cc832b13eca76d1eddcfc256290593e0779
Grant Holle <hollegrant@gmail.com>
2016-10-18 10:07:26 +0800

    Updating index file with correct commands

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
 
-const commands = ['tv', 'movies']
-      args = process.argv.slice(2)
+const commands = ['tv', 'movies', 'tv-setup']
+      args = process.argv.slice(2),
+      colors = require('colors')
 
 if (args[0] && commands.indexOf(args[0]) !== -1) {
   require(`./lib/${args[0]}`)()
+} else if (args[0].toLowerCase() === 'help') {
+  console.log(`Available commands are: ${'tv'.green}, ${'movies'.green}, ${'tv-setup'.green}`)
 } else {
-  require('./lib/tv-setup')()
+  console.log(`Command '${args[0].red}' not found. Available commands are: ${'tv'.green}, ${'movies'.green}, ${'tv-setup'.green}`)
 }
