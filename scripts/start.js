cc20ba9df521208a22c4f76e0bde49d67509b11d
Grant Holle <hollegrant@gmail.com>
2016-10-31 09:49:41 +0800

    Changing daemon to pull movies every 10 minutes

--- scripts/start.js
+++ scripts/start.js
@@ -4,7 +4,8 @@
 const tv = require('../lib/tv'),
       movies = require('../lib/movies'),
       minutely = 60 * 1000,
+      tenMinutes = 10 * minutely
       hourly = 60 * minutely
 
 let tvInterval = setInterval(tv, hourly)
-let moviesInterval = setInterval(movies, minutely)
+let moviesInterval = setInterval(movies, tenMinutes)
