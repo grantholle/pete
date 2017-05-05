47833b627d910c0e196ceb24828c98ffa97e2b4f
Grant Holle <hollegrant@gmail.com>
2017-05-05 22:01:16 +0800

    Fixing finished download issue

--- scripts/download-finished.js
+++ scripts/download-finished.js
@@ -13,7 +13,6 @@ const winston = require('../lib/logger'),
       refresh = () => {
         // Refresh the library
         kodi('localhost', 9090).then(connection => {
-          winston.info('Connected to Kodi')
           connection.VideoLibrary.Scan().then(() => {
             winston.info('Scanned library')
             process.nextTick(() => {
@@ -29,59 +28,60 @@ mediaDb.db.get('select * from downloads where transmission_id = ?', [process.env
   // If the item doesn't exist in the db, else if the item is a tv show, else it's a movie
   if (!item) {
     msg = `${process.env.TR_TORRENT_NAME} has finished downloading.`
-  } else if (item.season) {
-    msg = `${item.show} ${label(item.season, item.episode)}, ${item.name}, has finished downloading.`
   } else {
-    msg = `${item.name} has finished downloading.`
+    // Set the transmission id to null to prevent problems later
+    mediaDb.db.run('update downloads set transmission_id = null where id = ?', [item.id])
+
+    if (item.season) {
+      msg = `${item.show} ${label(item.season, item.episode)}, ${item.name}, has finished downloading.`
+    } else {
+      msg = `${item.name} has finished downloading.`
+    }
   }
 
   // Send a notification
   winston.info(msg)
   notify(msg)
 
-  // Set the transmission id to null to prevent problems later
-  if (item) {
-    mediaDb.db.run('update downloads set transmission_id = null where id = ?', [item.id])
-
-    // Since right now Transmission doesn't honor unwanted files,
-    // we have to manually delete unwanted files
-    fs.stat(downloadPath, (err, stats) => {
-      if (err) {
-        return winston.error(err)
-      }
-
-      if (stats.isDirectory()) {
-        fs.readdir(downloadPath, (err, files) => {
-          if (err) {
-            return winston.error(`Reading ${downloadPath} failed`, err)
-          }
-
-          const toDelete = files.filter(filename => {
-            return (filename.search(/.(mkv|avi|mp4|srt|idx|sub)$/gi) === -1 ||
-                    p.basename(filename, p.extname(filename)).toUpperCase() === 'RARBG.COM' ||
-                    filename.search(/sample/gi) !== -1)
-          })
-
-          winston.info(`Deleting unwanted files: ${toDelete.join(', ')}`)
-
-          eachOfSeries(toDelete, (filename, i, cb) => {
-            fs.unlink(p.join(downloadPath, filename), err => {
-              if (err) {
-                winston.error(`Error deleting file ${p.join(downloadPath, filename)}`, err)
-              }
-
-              cb()
-            })
-          }, () => {
-            refresh()
-          })
-        })
-      } else {
-        refresh()
-      }
-    })
-  } else {
-    refresh()
-  }
-
+  refresh()
 })
+
+function removeUnwanted () {
+  // Since right now Transmission doesn't honor unwanted files,
+  // we have to manually delete unwanted files
+  fs.stat(downloadPath, (err, stats) => {
+    if (err) {
+      return winston.error(err)
+    }
+
+    if (stats.isDirectory()) {
+      fs.readdir(downloadPath, (err, files) => {
+        if (err) {
+          return winston.error(`Reading ${downloadPath} failed`, err)
+        }
+
+        const toDelete = files.filter(filename => {
+          return (filename.search(/.(mkv|avi|mp4|srt|idx|sub)$/gi) === -1 ||
+                  p.basename(filename, p.extname(filename)).toUpperCase() === 'RARBG.COM' ||
+                  filename.search(/sample/gi) !== -1)
+        })
+
+        winston.info(`Deleting unwanted files: ${toDelete.join(', ')}`)
+
+        eachOfSeries(toDelete, (filename, i, cb) => {
+          fs.unlink(p.join(downloadPath, filename), err => {
+            if (err) {
+              winston.error(`Error deleting file ${p.join(downloadPath, filename)}`, err)
+            }
+
+            cb()
+          })
+        }, () => {
+          refresh()
+        })
+      })
+    } else {
+      refresh()
+    }
+  })
+}
