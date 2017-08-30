6aa8dde0e85588e1b0f2515221db67fcae929e06
Grant Holle <hollegrant@gmail.com>
2017-08-30 16:17:39 +0200

    Use commander instead of parsing the commands manually

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
 #!/usr/bin/env node
 
-const commands = ['install', 'tv', 'movies', 'tv-setup', 'add-service-file']
-      args = process.argv.slice(2),
-      colors = require('colors')
+const program = require('commander')
 
-if (args[0] && commands.indexOf(args[0]) !== -1) {
-  require(`./lib/${args[0]}`)()
-} else if (args[0].toLowerCase() === 'help') {
-  console.log(`Available commands are: ${'tv'.green}, ${'movies'.green}, ${'tv-setup'.green}`)
-} else {
-  console.log(`Command '${args[0].red}' not found. Available commands are: ${commands.map(c => c.green).join(', ')}`)
-}
+program.version(require('./package.json').version)
+
+// Install command
+program
+  .command('install')
+  .alias('i')
+  .description('Sets up the local filesystem, authorizes TMdb api credentials, and other misc installation requirements')
+  .action(() => {
+    require(`./lib/install`)()
+  })
+
+program
+  .command('tv')
+  .description('Fetches your TMdb TV watchlist and finds new episodes of your shows')
+  .action(() => {
+    require(`./lib/tv`)()
+  })
+
+program
+  .command('movies')
+  .alias('m')
+  .description('Fetches your TMdb movie watchlist and finds them')
+  .action(() => {
+    require(`./lib/movies`)()
+  })
+
+program
+  .command('tv-setup')
+  .alias('s')
+  .description('Runs the configuation setup for the shows in your TV watchlist')
+  .action(() => {
+    require(`./lib/tv-setup`)()
+  })
+
+program
+  .command('add-service-file')
+  .alias('f')
+  .description('Saves a service file to run Pete as a service on boot')
+  .action(() => {
+    require(`./lib/add-service-file`)()
+  })
+
+
+program.parse(process.argv)
