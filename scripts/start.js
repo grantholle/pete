093d1d50e0d075bdf8bdb600f77591c2f3b7bf69
Grant Holle <hollegrant@gmail.com>
2017-11-09 09:25:03 +0800

    Split const declarations into multiple statements

--- scripts/start.js
+++ scripts/start.js
@@ -1,10 +1,9 @@
 #!/usr/bin/env node
 'use strict'
 
-const minutely = 60 * 1000,
-  cleanDelay = minutely * 2,
-  thirteenMinutes = 13 * minutely,
-  hourly = 60 * minutely
+const minutely = 60 * 1000
+const thirteenMinutes = 13 * minutely
+const hourly = 60 * minutely
 
 let tvInterval = setInterval(() => {
   require('../lib/tv')()
