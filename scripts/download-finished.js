fb5439499a8190483ea3c82990c333a858cc9efc
Grant Holle <hollegrant@gmail.com>
2017-11-20 22:24:54 +0800

    Rename files when they're finished, not when they're added
    Downloading was taking a super long time. I'm not entirely sure why, but I think it has to do with the renaming and adding from a file or something, I'm not sure. I had to add a torrents collection to store all the torrents and what their names should be when the media is scanned.

--- scripts/download-finished.js
+++ scripts/download-finished.js
@@ -1,11 +1,78 @@
 #!/usr/bin/env node
 'use strict'
 
+// process.env.TR_TORRENT_ID = 4
+
 const winston = require('../lib/logger')
 const notify = require('../lib/pushbullet').finished
+const retrieveDatabase = require('../lib/database')
+const transmission = require('../lib/transmission')
+const async = require('async')
+const p = require('path')
+const torrentId = parseFloat(process.env.TR_TORRENT_ID)
 
-let msg = `${process.env.TR_TORRENT_NAME} has finished downloading.`
+retrieveDatabase().then(database => {
+  const torrentsCollection = database.torrents
+  const db = database.db
+  const torrent = torrentsCollection.findOne({ transmission_id: torrentId })
 
-// Log the message and send a notification about what has been completed
-winston.info(msg)
-notify(msg)
+  if (!torrent) {
+    return winston.error(`Could not find id in the torrents collection ${process.env.TR_TORRENT_ID}`)
+  }
+
+  transmission.get(torrent.transmission_id, (err, torrents) => {
+    if (err) {
+      return winston.error(err)
+    }
+
+    if (!torrents.torrents[0]) {
+      return winston.error(`Could not get information for ${torrent.name}`)
+    }
+
+    const torrentInfo = torrents.torrents[0]
+    const files = torrentInfo.files
+
+    // Iterate over all the files and rename appropriately
+    // Will changing the directory name affect the other
+    // renaming because it is different by the time they are changed?
+    // It might need to be reversed to do the files first then the root directory
+    async.eachOfSeries(files, (file, index, callback) => {
+      let newName
+
+      // Rename it appropriately (TV episode or Movie title and year)
+      // We're only interested in video and subtitle files
+      // and video files that aren't samples
+      if (file.name.search(/.(mkv|avi|mp4|mov)$/gi) !== -1) {
+        newName = file.name.search(/(sample|rarbg\.com)/gi) === -1 ? torrent.name + p.extname(file.name) : `unwanted ${index}`
+      }
+
+      if (!newName) {
+        return callback()
+      }
+
+      transmission.rename(torrentInfo.id, file.name, newName, callback)
+    }, err => {
+      if (err) {
+        return winston.error(err)
+      }
+
+      // Rename the root folder
+      if (files[0].name.indexOf('/') !== -1) {
+        transmission.rename(torrentInfo.id, p.dirname(files[0].name), torrent.name, err => {
+          if (err) {
+            return winston.error(err)
+          }
+
+          let msg = `${torrent.name} has finished downloading.`
+
+          // Log the message and send a notification about what has been completed
+          winston.info(msg)
+          notify(msg)
+
+          torrentsCollection.remove(torrent)
+          db.saveDatabase()
+        })
+      }
+    })
+  })
+})
