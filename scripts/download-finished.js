0b8389fdd2ce75a41772ebd60fe00110513049a2
Grant Holle <hollegrant@gmail.com>
2017-11-21 08:00:39 +0800

    Clean up download finished script
    Added functionality to parse the torrent name even if there's not an entry in the database so renaming can still function properly

--- scripts/download-finished.js
+++ scripts/download-finished.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
 'use strict'
 
-// process.env.TR_TORRENT_ID = 4
-
 const winston = require('../lib/logger')
 const notify = require('../lib/pushbullet').finished
 const retrieveDatabase = require('../lib/database')
@@ -10,16 +8,29 @@ const transmission = require('../lib/transmission')
 const async = require('async')
 const p = require('path')
 const torrentId = parseFloat(process.env.TR_TORRENT_ID)
+const ptt = require('parse-torrent-title')
+const labelize = require('../lib/show-label')
 
+// Load database
 retrieveDatabase().then(database => {
   const torrentsCollection = database.torrents
   const db = database.db
-  const torrent = torrentsCollection.findOne({ transmission_id: torrentId })
+  let torrent = torrentsCollection.findOne({ transmission_id: torrentId })
+  const existingTorrent = !!torrent
 
-  if (!torrent) {
-    return winston.error(`Could not find id in the torrents collection ${process.env.TR_TORRENT_ID}`)
+  // If there wasn't an entry in our database
+  // Parse the name and make a prettier name
+  if (!existingTorrent) {
+    const parsed = ptt.parse(process.env.TR_TORRENT_NAME)
+    const name = parsed.season ? `${parsed.title} - ${labelize(parsed.season, parsed.episode)}` : `${parsed.title} (parsed.year)`
+
+    torrent = {
+      transmission_id: process.env.TR_TORRENT_ID,
+      name
+    }
   }
 
+  // Fetch the torrent info
   transmission.get(torrent.transmission_id, (err, torrents) => {
     if (err) {
       return winston.error(err)
@@ -33,15 +44,11 @@ retrieveDatabase().then(database => {
     const files = torrentInfo.files
 
     // Iterate over all the files and rename appropriately
-    // Will changing the directory name affect the other
-    // renaming because it is different by the time they are changed?
-    // It might need to be reversed to do the files first then the root directory
     async.eachOfSeries(files, (file, index, callback) => {
       let newName
 
       // Rename it appropriately (TV episode or Movie title and year)
-      // We're only interested in video and subtitle files
-      // and video files that aren't samples
+      // We're only interested in non-sample video files
       if (file.name.search(/.(mkv|avi|mp4|mov)$/gi) !== -1) {
         newName = file.name.search(/(sample|rarbg\.com)/gi) === -1 ? torrent.name + p.extname(file.name) : `unwanted ${index}`
       }
@@ -58,20 +65,18 @@ retrieveDatabase().then(database => {
 
       // Rename the root folder
       if (files[0].name.indexOf('/') !== -1) {
-        transmission.rename(torrentInfo.id, p.dirname(files[0].name), torrent.name, err => {
-          if (err) {
-            return winston.error(err)
-          }
+        transmission.rename(torrentInfo.id, p.dirname(files[0].name), torrent.name, winston.error)
+      }
 
-          let msg = `${torrent.name} has finished downloading.`
+      // Log the message and send a notification about what has been completed
+      let msg = `${torrent.name} has finished downloading.`
+      winston.info(msg)
+      notify(msg)
 
-          // Log the message and send a notification about what has been completed
-          winston.info(msg)
-          notify(msg)
-
-          torrentsCollection.remove(torrent)
-          db.saveDatabase()
-        })
+      // Remove the entry and save the database
+      if (existingTorrent) {
+        torrentsCollection.remove(torrent)
+        db.saveDatabase()
       }
     })
   })
